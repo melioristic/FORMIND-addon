a6070a998b49e1eaaacfb438e55926da1bc2e541
Mohit Anand <itsmohitanand@gmail.com>
2020-11-23 12:48:09 +0100

    initial setup

---
+++ forpy/graphics.py
@@ -0,0 +1,10 @@
+#
+# Created on Mon Nov 23 2020
+#
+# Copyright (c) 2020 Mohit Anand
+#
+# For any details contact itsmohitanand@gmail.com 
+#
+
+
+## The goal of this script is to create plots from the output of forest model with mininum dependencies. 
