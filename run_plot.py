55e72de16ea7869fd67768c858ca6fcc3c61f09c
melioristic <itsmohitanand@gmail.com>
2020-12-15 14:50:26 +0100

    change name

--- run_plot.py
+++ run_plot.py
@@ -2,12 +2,12 @@ from forpy import prep_climate_cflux, plot_climate_cflux
 
 
 project_path = "Projects/Project_Madagascar_Betampona/"
-climate_file = 'percentage-change_DUU_perc_80_climate_400y.txt'
+climate_file = 'percentage-change_UUD_perc_30_climate_400y.txt'
 
 cflux_file = "madagascar_"+climate_file.split('.')[0]+'.cflux'
 cflux_path = project_path+'results/'+cflux_file
 
 climate_path = project_path+'formind_parameters/Climate/'+climate_file
 
-nee_arr, data_climate, time = prep_climate_cflux(cflux_path, climate_path, 10)
+nee_arr, data_climate, time = prep_climate_cflux(cflux_path, climate_path, 1)
 plot_climate_cflux(nee_arr, data_climate, time)
