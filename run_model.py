1e05082e146b0403534d35118c4d06754c12f372
melioristic <itsmohitanand@gmail.com>
2020-12-15 13:14:32 +0100

    0.8 climate files

--- run_model.py
+++ run_model.py
@@ -3,16 +3,16 @@ from forpy import Formind
 model_path = ''	
 par_file_name='madagascar'
 project_path='Projects/Project_Madagascar_Betampona/'
-num_sim = 1
+num_sim = 10
 climate_file_ori = 'climate_400y.txt'
 
 
 
 
 scenario = 'fractional-change'
-climate_file = 'fractional-change_DID_frac_0.5_climate_400y.txt'
+climate_file = 'fractional-change_DID_frac_0.8_climate_400y.txt'
 sim_id = '_'.join(climate_file.split('_')[:4])
 
 model = Formind(model_path, project_path, par_file_name)
-# model.generate_scenario_climate(climate_file_ori, scenario, frac=0.5)
+# model.generate_scenario_climate(climate_file_ori, scenario, frac=0.8)
 model.run(sim_id=sim_id, num_sim=num_sim)
--- run_plot.py
+++ run_plot.py
@@ -2,11 +2,11 @@ from forpy import prep_climate_cflux, plot_climate_cflux
 
 
 project_path = "Projects/Project_Madagascar_Betampona/"
-climate_file = 'fractional-change_DID_frac_0.5_climate_400y.txt'
+climate_file = 'fractional-change_DID_frac_0.8_climate_400y.txt'
 
 cflux_file = "madagascar_"+'_'.join(climate_file.split('_')[:4])+'.cflux'
-cflux_path = project_path+'results/madagascar_fractional-change_DID_frac_0.5.cflux'
+cflux_path = project_path+'results/madagascar_fractional-change_DID_frac_0.8.cflux'
 climate_path = project_path+'formind_parameters/Climate/'+climate_file
 
-nee_arr, data_climate, time = prep_climate_cflux(cflux_path, climate_path, 1)
+nee_arr, data_climate, time = prep_climate_cflux(cflux_path, climate_path, 10)
 plot_climate_cflux(nee_arr, data_climate, time)
